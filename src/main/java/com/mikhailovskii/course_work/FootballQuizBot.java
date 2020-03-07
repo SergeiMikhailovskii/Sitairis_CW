226732ca735c355676c921f26ca1175c0a1ec0a2
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-03-07 17:09:25 +0300

    Added Bot skeleton

---
+++ src/main/java/com/mikhailovskii/course_work/FootballQuizBot.java
@@ -0,0 +1,42 @@
+package com.mikhailovskii.course_work;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.List;
+
+public class FootballQuizBot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            System.out.println(update.getMessage().getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "FootballQuizCourseWorkBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "856507021:AAHucrT_sB9Q6c1wd-Bzf32GX9sUo7q5vQA";
+    }
+
+}
---
+++ src/main/java/com/mikhailovskii/course_work/Main.java
@@ -0,0 +1,21 @@
+package com.mikhailovskii.course_work;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try {
+            botsApi.registerBot(new FootballQuizBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
