98d8d7d55692910bdaa9b3898a1f619da192beb3
Sergei Mikhailovskii <mikhailovskii.s@tut.by>
2020-03-08 19:18:39 +0300

    Added buttons

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java -cp target/classes/:target/dependency/* com.mikhailovskii.course_work.Main
--- src/main/java/com/mikhailovskii/course_work/FootballQuizBot.java
+++ src/main/java/com/mikhailovskii/course_work/FootballQuizBot.java
@@ -3,8 +3,11 @@ package com.mikhailovskii.course_work;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FootballQuizBot extends TelegramLongPollingBot {
@@ -12,21 +15,17 @@ public class FootballQuizBot extends TelegramLongPollingBot {
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
+            System.out.println(update.getMessage().getText());
             SendMessage message = new SendMessage()
                     .setChatId(update.getMessage().getChatId())
-                    .setText(update.getMessage().getText());
-            System.out.println(update.getMessage().getText());
+                    .setText(update.getMessage().getText())
+                    .setReplyMarkup(getMainMenuKeyboard());
             try {
                 execute(message);
             } catch (TelegramApiException e) {
                 e.printStackTrace();
             }
         }
-    }
-
-    @Override
-    public void onUpdatesReceived(List<Update> updates) {
-
     }
 
     @Override
@@ -37,6 +36,24 @@ public class FootballQuizBot extends TelegramLongPollingBot {
     @Override
     public String getBotToken() {
         return "856507021:AAHucrT_sB9Q6c1wd-Bzf32GX9sUo7q5vQA";
+    }
+
+    private static ReplyKeyboardMarkup getMainMenuKeyboard() {
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
+
+        List<KeyboardRow> keyboardRows = new ArrayList<>();
+        KeyboardRow row = new KeyboardRow();
+        row.add("One");
+        keyboardRows.add(row);
+        row = new KeyboardRow();
+        row.add("Two");
+        keyboardRows.add(row);
+        replyKeyboardMarkup.setKeyboard(keyboardRows);
+
+        return replyKeyboardMarkup;
     }
 
 }
